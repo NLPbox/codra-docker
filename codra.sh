a8ae172bebad708b62cb98ef7fc01075d5eac69c
Arne Neumann <nlpbox.programming@arne.cl>
2018-05-21 23:42:46 +0200

    feat: cli wrapper is now path-agnostic

--- codra.sh
+++ codra.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+CURRENTDIR=$(pwd)
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+INPUT_BASENAME=$(basename $1)
+
+cd $SCRIPTDIR
+
 python Discourse_Segmenter.py $1 > $1.edu
 python Discourse_Parser.py $1.edu
-mv tmp_doc.dis $1.dis
-cat $1.dis
+
+cd $CURRENTDIR
+
+mv $SCRIPTDIR/tmp_doc.dis $INPUT_BASENAME.dis
+cat $INPUT_BASENAME.dis
